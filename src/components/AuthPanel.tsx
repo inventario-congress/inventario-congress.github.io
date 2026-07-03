29a7eafd0af5a0f39f637c7a52161448fa17787d
Ernesto Novillo <ernesto.novillo@gmail.com>
2026-07-03 00:37:00 -0400

    useMemo wasn't used. Now it's removed.

--- src/components/AuthPanel.tsx
+++ src/components/AuthPanel.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useState } from 'react'
+import { useEffect, useState } from 'react'
 import type { Messages } from '../i18n'
 import { isSupabaseConfigured, supabase } from '../supabaseClient'
 
